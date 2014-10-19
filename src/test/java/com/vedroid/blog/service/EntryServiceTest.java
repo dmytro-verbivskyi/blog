8e6497a0b118e7dec55dcbf04ed9f89661ca4420
dverbovskiy <dverbovskiy@gmail.com>
2014-10-19 23:08:06 +0300

    second test, delete old way for testing from main method

--- src/test/java/com/vedroid/blog/service/EntryServiceTest.java
+++ src/test/java/com/vedroid/blog/service/EntryServiceTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class EntryServiceTest extends InMemoryDBTest {
 
     @Test
-    public void firstTest() {
+    public void simpleCreate() {
         String testName = "Michael";
         Entry contact = new Entry();
         contact.setName(testName);
@@ -21,6 +21,13 @@ public class EntryServiceTest extends InMemoryDBTest {
         Assert.assertNotNull(entries);
         Assert.assertEquals(1, entries.size());
         Assert.assertEquals(testName, entries.get(0).getName());
+    }
+
+    @Test
+    public void simpleFind() {
+        List<Entry> entries = entryService.findAll();
+        Assert.assertNotNull(entries);
+        Assert.assertEquals(0, entries.size());
     }
 
 }
