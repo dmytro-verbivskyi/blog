9fb444a07e159678cb8e2a1d1a90c484d17a1689
dverbovskiy <dverbovskiy@gmail.com>
2014-10-26 01:29:07 +0300

    locale is retrieved dynamically from properties file.

--- src/main/java/com/vedroid/blog/utils/MessageHelperImpl.java
+++ src/main/java/com/vedroid/blog/utils/MessageHelperImpl.java
@@ -3,6 +3,7 @@ package com.vedroid.blog.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.support.AbstractMessageSource;
 import org.springframework.stereotype.Component;
@@ -14,17 +15,11 @@ public class MessageHelperImpl implements MessageHelper {
 
     private static final Logger log = LoggerFactory.getLogger(MessageHelperImpl.class);
 
-//    @Value("${locale.code}")
-//    private String localePropertyCode;
+    @Value("${locale.code}")
+    private String localePropertyCode;
 
     @Autowired
     private AbstractMessageSource messageSource;
-
-    private Locale defaultLocale = Locale.forLanguageTag("en");
-
-//    public MessageHelperImpl() {
-//        defaultLocale = Locale.forLanguageTag(localePropertyCode);
-//    }
 
     public AbstractMessageSource getMessageSource() {
         return messageSource;
@@ -32,8 +27,10 @@ public class MessageHelperImpl implements MessageHelper {
 
     @Override
     public String getMessage(String code, Object... params) {
+        final Locale locale = Locale.forLanguageTag(localePropertyCode);
+
         try {
-            return messageSource.getMessage(code, params, defaultLocale);
+            return messageSource.getMessage(code, params, locale);
         } catch (NoSuchMessageException e) {
             log.error(e.getMessage(), e);
         }
